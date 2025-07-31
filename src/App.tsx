f473b0c06de60120eea4f9b2dc0a8b7fe6f91cd8
jacob <yomu85@gmail.com>
2025-07-31 23:40:28 +0900

    #3 배포 이슈 대응

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import '@/global.css'
 import Main from '@/components/Main/index.tsx'
 import Header from '@/components/Header/index.tsx'
 import Footer from '@/components/Footer/index.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Header />
-    <Main />
-    <Footer />
-  </StrictMode>,
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
+
+export default App
