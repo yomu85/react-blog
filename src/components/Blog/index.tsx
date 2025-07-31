56cf140351e1e992ceecaa5223abc0778e006f89
jacob <yomu85@gmail.com>
2025-08-01 00:22:30 +0900

    #4 main컴포넌트 추가, isShowMoreButton 개수 변화 제어 추가

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -13,16 +13,17 @@ import { Trash2, ChevronDown } from "lucide-react";
 import styles from "./blog.module.css";
 
 interface IBlogProps extends React.HTMLAttributes<HTMLDivElement> {
-  blogLength: number;
+  blogCount: number;
 }
 
-const Blog = ({ blogLength }: IBlogProps) => {
+const Blog = ({ blogCount }: IBlogProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [openStates, setOpenStates] = useState<boolean[]>(
-    Array.from({ length: blogLength }, () => false)
+    Array.from({ length: blogCount }, () => false)
   );
 
-  const displayBlogLength = isExpanded ? blogLength : 1;
+  const displayBlogCount: number = isExpanded ? blogCount : 1;
+  const isShowMoreButton: boolean = blogCount > 1;
 
   useEffect(() => {
     setOpenStates((prev) => {
@@ -35,23 +36,28 @@ const Blog = ({ blogLength }: IBlogProps) => {
   return (
     <article className={styles.blog}>
       <div className={styles["blog-header"]}>
-        <strong>클래시로얄 전투 전략 블로그 수: {blogLength}개</strong>
-        <Button
-          variant="ghost"
-          style={{ width: "fit-content" }}
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          더보기
-          <ChevronDown className={isExpanded ? "rotate-180" : ""} />
-        </Button>
+        <strong>클래시로얄 전투 전략 블로그 수: {blogCount}개</strong>
+        {
+          isShowMoreButton && (
+            <Button
+              variant="ghost"
+              style={{ width: "fit-content" }}
+              onClick={() => setIsExpanded(!isExpanded)}
+            >
+              더보기
+              <ChevronDown className={isExpanded ? "rotate-180" : ""} />
+            </Button>
+          )
+        }
+        
       </div>
       <div className={styles["blog-content"]}>
-        {Array.from({ length: displayBlogLength }, (_, index) => {
+        {Array.from({ length: displayBlogCount }, (_, index) => {
           const isOpen = openStates[index];
           return (
             <Card key={index} className="w-full max-w-sm">
               <CardHeader>
-                <CardTitle className="flex items-center gap-2">
+                <CardTitle className="flex justify-between items-center text-lg gap-1">
                   클래시로얄 전투 전략 {index + 1}일차
                   <Button
                     variant="ghost"
@@ -68,9 +74,15 @@ const Blog = ({ blogLength }: IBlogProps) => {
                   </Button>
                 </CardTitle>
               </CardHeader>
-              <CardContent className={isOpen ? "block" : "hidden"}>
+              <CardContent
+                className={isOpen ? "flex" : "hidden"}
+                style={{
+                  flexDirection: "column",
+                  gap: "12px",
+                }}
+              >
                 <CardDescription>
-                  평시에는 고블린 2마리부터 세팅 후 카드를 돌리자
+                  초반에 고블린 카드 2장을 모아 승급하여 클랜카드를 모아가자
                 </CardDescription>
                 <Button variant="outline" className="w-full">
                   <Trash2 /> 삭제
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,28 @@
 import Blog from "@/components/Blog";
 import styles from "./main.module.css";
+import { Input } from "@/components/ui/input";
+import { useRef, useState } from "react";
 
 const Main = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [count, setCount] = useState(1);
+  
   return (
     <main className={styles.main}>
-      <Blog blogLength={6} />
+      <div className={styles["main-header"]}>
+        <h2>블로그 개수 지정하기</h2>
+        <Input 
+          ref={inputRef} 
+          min={1}
+          max={100}
+          type="number"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+          className={styles["main-input"]}
+        />
+        개
+      </div>
+      <Blog blogCount={count} />
     </main>
   );
 };
